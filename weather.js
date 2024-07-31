b20f73c1f6cd5f313bddefcc48bdfae828401bc1
usail7860 <usail7860@users.noreply.github.com>
2024-08-01 01:56:35 +0530

    update

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 const apikey="ee47733e05d812c8bca112465a152019";
-const apiurl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q="
+const apiurl="https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=ee47733e05d812c8bca112465a152019&q="
 
 const searchinput=document.querySelector(".search input");
 const searchbtn=document.querySelector(".search button");
@@ -8,14 +8,21 @@ const searchbtn=document.querySelector(".search button");
 
 async function weatherdata(city){
 
-    const reply=await fetch(apiurl+city+`&appid=${apikey}`);
+    const reply=await fetch(apiurl+city);
     const data= await reply.json();
     console.log(data);
           if(reply.status===404){
+            searchinput.value="";
             document.querySelector(".weather").style.display="none";
-alert("city not found");
-  searchinput.value=""
+            document.getElementById("errors").innerHTML="City Not Found";
           }
+          else if(searchinput.value==="")
+            {
+                document.getElementById("errors").innerHTML="";
+             alert("Please enter city name");
+    
+             document.querySelector(".weather").style.display="none";
+            }
           else{
     document.querySelector(".weather").style.display="block";
     document.querySelector(".city").innerHTML=data.name;
